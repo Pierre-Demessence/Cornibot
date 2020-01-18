b97cd60f75575fc4083996e85f52f6fecd2620bf
Pierre Demessence <pierre.demessence@gmail.com>
2020-01-18 21:26:28 +0100

    KarmaObserver actually give karma

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -2,7 +2,18 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     discordID: String,
-    nbMessages: Number
+    nbMessages: {
+        type: Number,
+        default: 0
+    },
+    karmaReceived: {
+        type: Number,
+        default: 0
+    },
+    karmaGiven: {
+        type: Number,
+        default: 0
+    }
 });
 
 export default mongoose.model("User", userSchema);
--- src/Observers/KarmaObserver.ts
+++ src/Observers/KarmaObserver.ts
@@ -3,6 +3,7 @@ import { CommandoClient } from "discord.js-commando";
 import { Message } from "discord.js";
 
 import { thanksWords } from "../Engine/Config";
+import User from "../Models/User";
 
 export default class KarmaObserver extends Observer {
     constructor(client: CommandoClient) {
@@ -12,12 +13,16 @@ export default class KarmaObserver extends Observer {
         });
     }
 
-    public Run(message: Message): void {
+    public async Run(message: Message): Promise<void> {
         const receivers = message.mentions.users.filter(user => {
             return !user.bot && message.author.id !== user.id;
         });
         if (receivers.size === 0) return;
-        message.channel.send(
+
+        await User.updateOne({ discordID: message.author.id }, { $inc: { karmaGiven: receivers.size } }, { upsert: true });
+        receivers.forEach(async u => await User.updateOne({ discordID: u.id }, { $inc: { karmaReceived: 1 } }, { upsert: true }));
+
+        await message.channel.send(
             `**${message.author.username}** gave karma to **${receivers
                 .array()
                 .map(u => u.username)
