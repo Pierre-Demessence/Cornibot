0368c9d2cf70dce292b57685b070d9370295d02b
Pierre Demessence <pierre.demessence@gmail.com>
2020-01-29 02:51:28 +0100

    Commands can now check for roles

--- src/Engine/CorniCommand.ts
+++ src/Engine/CorniCommand.ts
@@ -1,9 +1,23 @@
-import { Command, CommandInfo } from "discord.js-commando";
+import { Command, CommandInfo, CommandoMessage } from "discord.js-commando";
 import DiscordBot from "./DiscordBot";
+
+interface CorniCommandInfo extends CommandInfo {
+    allowedRoles?: string[];
+}
 
 export default abstract class CorniCommand extends Command {
     public client!: DiscordBot;
-    constructor(client: DiscordBot, info: CommandInfo) {
+    public allowedRoles: string[];
+
+    constructor(client: DiscordBot, info: CorniCommandInfo) {
         super(client, info);
+        this.allowedRoles = info.allowedRoles || [];
+    }
+
+    hasPermission(message: CommandoMessage, ownerOverride: boolean): boolean | string {
+        const res = super.hasPermission(message, ownerOverride);
+        if (!res) return res;
+        if (this.allowedRoles.length > 0) return this.allowedRoles.some(role => message.member.roles.has(role));
+        return true;
     }
 }
