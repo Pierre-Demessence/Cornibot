0870de6a3e71182b1c5924ea5d76628f2a40ef69
Pierre Demessence <pierre.demessence@gmail.com>
2020-01-30 01:09:30 +0100

    Better UnmuteService

--- src/Services/UnmuteService.ts
+++ src/Services/UnmuteService.ts
@@ -8,27 +8,35 @@ import Logger from "../Utils/Logger";
 import DiscordBot from "../Engine/DiscordBot";
 
 export default class UnmuteService extends Service {
-    public int = 0;
+    private timers: Map<Mute, NodeJS.Timer> = new Map();
     constructor(client: DiscordBot) {
         super(client, {
             name: "unmute"
         });
     }
 
+    public async Unmute(mute: Mute): Promise<void> {
+        const member = await this.client.GetGuild().members.fetch(mute.user);
+        if (!member?.roles.has(Config.mutedRoleID)) {
+            Logger.debug(`${member?.user.tag} was already unmuted.`);
+            return;
+        }
+        const channel = this.client.GetGuild().channels.get(mute.channel) as TextChannel;
+        await member?.roles.remove(Config.mutedRoleID);
+        const timer = this.timers.get(mute);
+        if (timer) this.client.clearTimeout(timer);
+        channel.send(`${member?.user.tag} has been unmuted.`);
+        Logger.debug(`${member?.user.tag} has been unmuted.`);
+    }
+
     public StartTimer(mute: Mute): void {
         const milliseconds = moment(mute.dateEnd).diff(moment(), "milliseconds");
         Logger.debug(`Starting an Unmute Timer for ${milliseconds}ms`);
-        setTimeout(async () => {
-            const channel = this.client.GetGuild().channels.get(mute.channel) as TextChannel;
-            const member = await this.client.GetGuild().members.fetch(mute.user);
-            channel.send(`${member?.user.tag} has been unmuted.`);
-            Logger.debug(`${member?.user.tag} has been unmuted.`);
-            await member?.roles.remove(Config.mutedRoleID);
-        }, milliseconds);
+        const timer = this.client.setTimeout(() => this.Unmute(mute), milliseconds);
+        this.timers.set(mute, timer);
     }
 
     public async Run(): Promise<void> {
-        this.int = 1;
         const mutes = await MuteModel.find({
             dateEnd: {
                 $gt: new Date()
