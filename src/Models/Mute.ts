baabe884b78f8dde48d49a3e9de6bf464a1da4b5
Pierre Demessence <pierre.demessence@gmail.com>
2020-05-26 01:42:44 +0200

    Fixed props

--- src/Models/Mute.ts
+++ src/Models/Mute.ts
@@ -5,17 +5,17 @@ import { User } from "./User";
 import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
 
 export class Mute extends TimeStamps {
-    @prop({ required: true, ref: User, refType: mongoose.Schema.Types.String })
+    @prop({ required: true, ref: User, type: mongoose.Schema.Types.String })
     user!: Ref<User>;
-    @prop({ required: true, ref: User, refType: mongoose.Schema.Types.String })
+    @prop({ required: true, ref: User, type: mongoose.Schema.Types.String })
     author!: Ref<User>;
     @prop({ default: false })
     finished!: boolean;
-    @prop()
+    @prop({ required: true })
     dateEnd!: Date;
     @prop()
     reason!: string;
-    @prop()
+    @prop({ required: true })
     channel!: string;
 }
 
