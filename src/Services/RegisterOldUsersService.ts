b30ab94fc13328e0acfea7a3a2ebd5aca76905d4
Pierre Demessence <pierre.demessence@gmail.com>
2020-05-26 01:53:35 +0200

    Fixed old user registration

--- src/Services/RegisterOldUsersService.ts
+++ src/Services/RegisterOldUsersService.ts
@@ -13,9 +13,7 @@ export default class RegisterOldUsersService extends Service {
     public async Run(): Promise<void> {
         Logger.silly("Registering current users...");
         const members = await this.client.GetGuild().members.fetch();
-        for (const key in members) {
-            const member = members.get(key);
-            if (!member) continue;
+        for (const [, member] of members) {
             await User.findByIdAndUpdate(
                 member.id,
                 {},
