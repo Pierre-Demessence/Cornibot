a75b1d5c01061147f735636483c8add730ca64f1
Pierre Demessence <pierre.demessence@gmail.com>
2020-05-25 03:20:37 +0200

    Better handling of uncaught errors/exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 import "reflect-metadata";
 require("dotenv-safe").config();
 
-import Cornibot from "./Engine/CorniBot";
 import moment from "moment";
 
+import Cornibot from "./Engine/CorniBot";
+import Logger from "./Utils/Logger";
+
 moment.locale("en");
+
+process.on("uncaughtException", async (err) => {
+    Logger.error(`Uncaught Exception: ${err.stack}`);
+    if (process.env.NODE_ENV === "production") return;
+    Logger.error(`Exiting with status 1 in 3s`);
+    setTimeout(() => process.exit(1), 3000);
+});
+process.on("unhandledRejection", async (err) => {
+    Logger.error(`Unhandled Rejection: ${(err as any).stack || err}`);
+    if (process.env.NODE_ENV === "production") return;
+    Logger.error(`Exiting with status 1 in 3s`);
+    setTimeout(() => process.exit(1), 3000);
+});
 
 new Cornibot().Start();
